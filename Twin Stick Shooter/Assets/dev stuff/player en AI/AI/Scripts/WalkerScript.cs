a9b98e5230432ba4aef8697c8610e7ef184cba98
Jelle van der Schaaf <jellesimonvds@gmail.com>
2018-04-19 10:21:13 +0200

    Speler en AI

---
+++ Twin Stick Shooter/Assets/dev stuff/player en AI/AI/Scripts/WalkerScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class WalkerScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Target, m_StartPoint;
+    private NavMeshAgent m_NavMeshAgent;
+    [SerializeField]
+    private GameObject Bullet_Emitter;
+    [SerializeField]
+    private GameObject Kogel;
+    [SerializeField]
+    private GameObject Speler;
+    [SerializeField]
+    private float Bullet_Forward_Force;
+    float timer;
+    private bool WalkerStart = true;
+    bool WalkerDuring = false;
+
+    RaycastHit m_RayHit;
+
+    void Start()
+    {
+        m_NavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private float Distance(Vector3 objectA, Vector3 objectb)
+    {
+        return Vector3.Distance(objectA, objectb);
+    }
+    void Update()
+    {
+        if (WalkerStart == true)
+        {
+            m_NavMeshAgent.SetDestination(m_StartPoint.position);
+        }
+        if (WalkerDuring == true)
+        {
+            if (timer >= 10)
+            {
+                m_NavMeshAgent.SetDestination(m_StartPoint.position);
+                timer = 0;
+                WalkerDuring = false;
+            }
+        }
+
+        Vector3 targetDir = m_Target.position - transform.position;
+
+        float angleToPlayer = (Vector3.Angle(targetDir, transform.forward));
+
+
+        //&& angleToPlayer >= -60 && angleToPlayer <= 60
+        //Debug.Log(m_RayHit.collider.name);
+            Debug.DrawLine(transform.position, m_RayHit.point, Color.green);
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector3 Direction = (m_Target.position - transform.position).normalized;
+        if (Physics.Raycast(transform.position, Direction, out m_RayHit, 15f))
+        {
+            if (m_RayHit.collider.tag == "Player")
+            {
+                m_NavMeshAgent.SetDestination(m_Target.position);
+                WalkerStart = false;
+                if (Distance(transform.position, m_Target.position) < 15f)
+                {
+                    timer += 1 * Time.deltaTime;
+                    if (timer >= 1)
+                    {
+                        GameObject Temporary_Bullet_Handler;
+                        Temporary_Bullet_Handler = Instantiate(Kogel, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+                        Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+                        Rigidbody Temporary_RigidBody;
+                        Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+                        Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+                        Destroy(Temporary_Bullet_Handler, 25f);
+                        timer = 0;
+                    }
+                }
+            }
+            else
+            {
+                WalkerDuring = true;
+            }
+        }
+        Debug.DrawRay(transform.position, Direction * 15f, Color.red);
+    }
+}
+
---
+++ Twin Stick Shooter/Assets/dev stuff/player en AI/player/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float moveSpeed;
+    private Rigidbody PlayerRigid;
+
+    private Vector3 MoveInput;
+    private Vector3 MoveVelocity;
+
+    private Camera MainCamera;
+
+    [SerializeField]
+    private GameObject Kogel;
+    [SerializeField]
+    private GameObject Bullet_Emitter;
+    [SerializeField]
+    private float Bullet_force;
+    int timer;
+	void Start ()
+    {
+        PlayerRigid = GetComponent<Rigidbody>();
+        MainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MoveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        MoveVelocity = MoveInput * moveSpeed;
+
+        Ray CameraRay = MainCamera.ScreenPointToRay(Input.mousePosition);
+
+        Plane worldPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+        if (worldPlane.Raycast(CameraRay, out rayLength))
+        {
+            Vector3 pointToLook = CameraRay.GetPoint(rayLength);
+            Debug.DrawLine(CameraRay.origin, pointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+        print(timer);
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerRigid.velocity = MoveVelocity;
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            timer++;
+            if (timer >= 1)
+            {
+                GameObject Temporary_Bullet_Handler;
+                Temporary_Bullet_Handler = Instantiate(Kogel, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+                Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+                Rigidbody Temporary_RigidBody;
+                Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+                Temporary_RigidBody.AddForce(transform.forward * Bullet_force);
+
+                Destroy(Temporary_Bullet_Handler, 25f);
+                timer = 0;
+            }
+
+        }
+    }
+}
